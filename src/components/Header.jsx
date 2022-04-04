71546ed48d6e7a072a7929e5c348164ddfaab66e
StorchAndrii <andrey.st.light@gmail.com>
2022-04-04 19:22:20 +0300

    Lesson3-разбивка на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/RPC-JP_Logo.png/600px-RPC-JP_Logo.png"
+      alt="logo"
+    />
+  </header>
+  )
+}
+
+export default Header
