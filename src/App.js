4aff3ccb80e4a909831454ebec27e9d86457f473
StorchAndrii <andrey.st.light@gmail.com>
2022-04-04 17:14:48 +0300

    Import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Technologies from "./Technologies";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Technologies/>
+      <Header />
+      <Technologies />
+      <Footer/>
     </div>
   );
 }
