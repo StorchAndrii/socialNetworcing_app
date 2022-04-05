30712f6ec24019b947721a7e3c51feab9f082fca
StorchAndrii <andrey.st.light@gmail.com>
2022-04-05 21:42:31 +0300

    Dial

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import s from "./Dialogs.module.css";
 
-
 const Dialogs = () => {
-  return <div>
-      Message
-  </div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Dmitrii</div>
+        <div className={s.dialog}>Andrey</div>
+        <div className={s.dialog}>Stepan</div>
+        <div className={s.dialog}>Viktor</div>
+        <div className={s.dialog}>Lev</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi!</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>YO!</div>
+        <div className={s.message}>Bye-bye</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
