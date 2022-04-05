0bf0a79a73da5715169ebf443d04083ff9c4b8c3
StorchAndrii <andrey.st.light@gmail.com>
2022-04-06 00:43:28 +0300

    DialogPages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,52 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Dialogs = () => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog}>Dmitrii</div>
-        <div className={s.dialog}>Andrey</div>
-        <div className={s.dialog}>Stepan</div>
-        <div className={s.dialog}>Viktor</div>
-        <div className={s.dialog}>Lev</div>
+        <div className={`${s["dialog"]} ${s["active"]}`}>
+          <NavLink
+            to="/dialogs/1"
+            // className={(navData) => (navData.isActive ? s.activeLink : "")}
+          >
+            Dmitrii
+          </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink
+            to="/dialogs/2"
+            // className={(navData) => (navData.isActive ? s.activeLink : "")}
+          >
+            Andrey
+          </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink
+            to="/dialogs/3"
+            // className={(navData) => (navData.isActive ? s.activeLink : "")}
+          >
+            Stepan
+          </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink
+            to="/dialogs/4"
+            // className={(navData) => (navData.isActive ? s.activeLink : "")}
+          >
+            Viktor
+          </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink
+            to="/dialogs/5"
+            // className={(navData) => (navData.isActive ? s.activeLink : "")}
+          >
+            Lev
+          </NavLink>
+        </div>
       </div>
       <div className={s.messages}>
         <div className={s.message}>Hi!</div>
---
+++ src/components/Dialogs/messages/Messages.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+// import s from "./Messages.modules.css";
+
+// const Messages = () => {
+//   return (
+//     <div className={s.messages}>
+//       <div className={s.message}>Hi!</div>
+//       <div className={s.message}>How are you?</div>
+//       <div className={s.message}>YO!</div>
+//       <div className={s.message}>Bye-bye</div>
+//     </div>
+//   );
+// };
+
+// export default Messages;
