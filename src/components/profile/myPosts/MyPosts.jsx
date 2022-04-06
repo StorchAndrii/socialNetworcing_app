dc075647074dbd81c7475c38f6bab06c879d3622
StorchAndrii <andrey.st.light@gmail.com>
2022-04-06 15:20:43 +0300

    array.map

--- src/components/profile/myPosts/MyPosts.jsx
+++ src/components/profile/myPosts/MyPosts.jsx
@@ -4,29 +4,21 @@ import AddPost from "./NewPost/AddPost";
 import Post from "./post/Post";
 
 const MyPosts = () => {
-  let postData = [
+  let posts = [
     { id: 1, name: "My 1 post!", message: "Hi, how are you?", likePost: 20 },
     { id: 2, name: "My 2 post!", message: "It`s my first post", likePost: 50 },
   ];
 
+  let postsElement = posts.map((p) => (
+    <Post namePost={p.name} message={p.message} likePost={p.likePost} />
+  ));
   return (
     <div className={s.postsBlock}>
       <h3>My Post</h3>
       <div>
         <AddPost />
       </div>
-      <div className={s.posts}>
-        <Post
-          namePost={postData[0].name}
-          message={postData[0].message}
-          likePost={postData[0].likePost}
-        />
-        <Post
-          namePost={postData[1].name}
-          message={postData[1].message}
-          likePost={postData[1].likePost}
-        />
-      </div>
+      <div className={s.posts}>{postsElement}</div>
     </div>
   );
 };
